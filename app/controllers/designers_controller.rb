e868e29537bd6dc3680b8f457bc462326380da68
Vovin Lonshin <vovin.lonshin@gmail.com>
2014-02-08 02:26:37 -0600

    Add Navigation routes

---
+++ app/controllers/designers_controller.rb
@@ -0,0 +1,4 @@
+class DesignersController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Akho::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+   
+  get "designers/new"
+  root  'static_pages#home'
+  
+    match '/home', to: 'static_pages#home', via: 'get'
+    match '/help',    to: 'static_pages#help',    via: 'get'
+    match '/about',   to: 'static_pages#about',   via: 'get'
+    match '/contact', to: 'static_pages#contact', via: 'get'
+    match '/signup', to: 'designers#new', via: 'get'
  
   
 end
